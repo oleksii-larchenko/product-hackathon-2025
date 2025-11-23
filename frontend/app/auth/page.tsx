7e324e1cd654741fc16ad204e7c2552f620b3ff4
Oleksii Larchenko <oleksii.larchen@gmail.com>
2025-11-23 10:54:16 +0200

    feat: create structure

---
+++ frontend/app/auth/page.tsx
@@ -0,0 +1,5 @@
+import { AuthPage } from "@/containers/auth-page/AuthPage";
+
+export default function Auth() {
+	return <AuthPage />;
+}
---
+++ frontend/containers/auth-page/AuthPage.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+import styles from "./styles.module.css";
+
+export const AuthPage: FC = () => {
+	return (
+		<div className={styles.wrapper}>
+			<h1>Auth Page</h1>
+		</div>
+	);
+};
